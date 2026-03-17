c993924ae02fe081e19bbe2038482d85194d249b
sambeckers <sam.beckers@outlook.com>
2026-03-18 00:00:15 +1100

    outputs binary files, compatible with batching logic from pipeline

--- src/utils/analysis_trace.f90
+++ src/utils/analysis_trace.f90
@@ -7,7 +7,7 @@
 module analysis
 !
 ! Analysis routine to generate data for chemistry
-! Tracks particles paths and store the evolution in one file per particle
+! Tracks particle paths and stores evolution in shared per-batch binary files
 !
 ! :References: None
 !
@@ -18,6 +18,7 @@ module analysis
 ! :Dependencies: None
 !
 
+ use iso_fortran_env, only: int64, real64
  use part,       only: maxp
  implicit none
  character(len=20), parameter, public :: analysistype = 'trace'
@@ -28,9 +29,12 @@ module analysis
  integer, allocatable :: iprev(:)
  logical :: done_init = .false.
  integer :: ntrack = 0
- integer, allocatable :: track_id(:)
  logical, allocatable :: mask(:)
- integer :: id_start = 1, id_end = 0, n_boundary = 0
+ integer :: id_start = 1, id_end = 0
+ integer :: n_batches = 0
+ integer, allocatable :: batch_for_id(:)
+ integer, allocatable :: batch_units(:)
+ character(len=256) :: batch_map_file = 'trace_batch_map.txt'
  character(len=256) :: dir
  character(len=256) :: cwd
  real(8), allocatable :: av_lookup(:)
@@ -54,14 +58,16 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
  integer       :: i, j, k, ierr, completed_iterations
  integer       :: iu=10,ios
  logical       :: iexist
- character(len=9) :: filename
- integer :: isize
+ integer :: batch_idx
+ integer(int64) :: pid_out
+ real(real64) :: time_out, x_out, y_out, z_out, density_out, temp_out, av_out
  integer(4) :: npart_av, dump_num, last_under
  integer(4), allocatable :: av_id_tmp(:)
  real(8), allocatable :: av_val_tmp(:)
  character(len=40) :: avfile
+ character(len=256) :: batch_file
  integer :: iu_av
- namelist /trace_config/ id_start, id_end, n_boundary
+ namelist /trace_config/ id_start, id_end, n_batches, batch_map_file
 
  if (.not.done_init) then
       allocate(one(maxp))
@@ -82,17 +88,18 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
       ! read trace configuration from trace.cfg
       open(iu, file='trace.cfg', status='old', action='read', iostat=ios)
       if (ios /= 0) call fatal(analysistype, &
-         "Could not open trace.cfg. Create it with: &trace_config id_start=1, id_end=10000, n_boundary=0 /")
+         "Could not open trace.cfg. Create it with: &trace_config id_start=1, id_end=10000, n_batches=1 /")
       read(iu, nml=trace_config, iostat=ios)
       if (ios /= 0) call fatal(analysistype, "Failed to read trace.cfg namelist &trace_config")
       close(iu)
       if (id_end < id_start) call fatal(analysistype, "trace.cfg: id_end must be >= id_start")
-      ntrack = id_end - id_start + 1
+      if (n_batches <= 0) call fatal(analysistype, "trace.cfg: n_batches must be > 0")
       print*, "Reading config from trace.cfg"
-      print*, "  Tracking particle ID range: ", id_start, " to ", id_end, " (", ntrack, " particles)"
-      print*, "  Skipping first ", n_boundary, " particles (boundary)"
+      print*, "  Tracking particle ID range: ", id_start, " to ", id_end
+      print*, "  Batch map file: ", trim(batch_map_file)
+      print*, "  Number of batches: ", n_batches
 
-      ! make directory to store individual particle trace files in current working directory
+      ! make directory to store batch trace binary files in current working directory
       dir = 'trace_output/'
       call getcwd(cwd)
       print *, "Creating trace_output directory in: ", trim(cwd)//'/'//trim(dir)
@@ -101,18 +108,40 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
          call system('mkdir '//dir)
       end if
 
-      allocate(track_id(ntrack))
       allocate(mask(maxp))
       allocate(av_lookup(maxp))
+      allocate(batch_for_id(maxp))
+      allocate(batch_units(n_batches))
       av_lookup = 0.0d0
-      ! fill track_id array from range
-      do i=1,ntrack
-         track_id(i) = id_start + i - 1
-      end do 
-      print*, "Tracking ", ntrack, " particles"
+      batch_for_id = -1
+
+      open(iu, file=trim(batch_map_file), status='old', action='read', iostat=ios)
+      if (ios /= 0) call fatal(analysistype, "Could not open batch map file listed in trace.cfg")
+      ntrack = 0
+      do
+         read(iu, *, iostat=ios) pid_out, batch_idx
+         if (ios /= 0) exit
+         if (pid_out >= 1_int64 .and. pid_out <= int(maxp, int64)) then
+            if (batch_idx >= 0 .and. batch_idx < n_batches) then
+               batch_for_id(int(pid_out)) = batch_idx
+               ntrack = ntrack + 1
+            endif
+         endif
+      enddo
+      close(iu)
+      if (ntrack <= 0) call fatal(analysistype, "Batch map contained zero valid tracked particles")
+
+      do i=1,n_batches
+         write(batch_file, '(a,"trace_batch_",i5.5,".bin")') trim(dir), i-1
+         open(50+i, file=trim(batch_file), access='stream', form='unformatted', status='replace', action='write', iostat=ios)
+         if (ios /= 0) call fatal(analysistype, "Could not create batch trace binary file")
+         batch_units(i) = 50 + i
+      enddo
+
+      print*, "Tracking ", ntrack, " particles from batch map"
       call init_eos(ieos, ierr)
       if (ierr /= 0) call fatal(analysistype, "Failed to initialise EOS")
-      print*, "=== Initialisation complete: tracking ", ntrack, " particles (IDs ", id_start, "-", id_end, ") ==="
+      print*, "=== Initialisation complete: tracking ", ntrack, " particles across ", n_batches, " batch files ==="
    
    else
       dt_cgs = (time - tprev)*utime
@@ -142,21 +171,30 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
          deallocate(av_id_tmp, av_val_tmp)
          print*, "  Loaded A_V for ", npart_av, " particles from "//trim(avfile)
       endif
-      ! update mask: track all particles whose original ID falls in [id_start, id_end]
+      ! update mask: track particles explicitly listed in batch map
       mask = .false.
       do j=1,npart
-         if (iorig(j) >= id_start .and. iorig(j) <= id_end) then
-            mask(j) = .true.
+         if (iorig(j) >= 1 .and. iorig(j) <= maxp) then
+            if (batch_for_id(iorig(j)) >= 0) mask(j) = .true.
+         endif
+      enddo
+      do j=1,npart
+         if (.not.mask(j)) then
+            cycle
+         endif
+         if (iorig(j) < id_start .or. iorig(j) > id_end) then
+            mask(j) = .false.
          endif
       enddo
       !$omp parallel do default(none) &
       !$omp shared(npart,xyzh,vxyzu,nprev,iorig,iorig_old,iprev,iverbose,dir) &
-      !$omp shared(particlemass,unit_density,mask,time,utime,iphase,n_boundary,id_start,id_end) &
-      !$omp shared(ieos,gamma,gmw,completed_iterations,av_lookup) &
-      !$omp private(i,j,k,rho_cgs,numberdensity,T_gas,gammai,mui,AV,filename,iu,isize)
+      !$omp shared(particlemass,unit_density,mask,time,utime,iphase,id_start,id_end) &
+      !$omp shared(ieos,gamma,gmw,completed_iterations,av_lookup,batch_for_id,batch_units,n_batches) &
+      !$omp private(i,j,k,rho_cgs,numberdensity,T_gas,gammai,mui,AV,iu,batch_idx) &
+      !$omp private(pid_out,time_out,x_out,y_out,z_out,density_out,temp_out,av_out)
       outer: do i=1,npart
          iu = 10
-         if (mask(i) .eqv. .true. .and. .not.isdead_or_accreted(xyzh(4,i))) then
+         if (mask(i) .and. .not.isdead_or_accreted(xyzh(4,i))) then
             ! never write particles outside configured ID range.
             if (iorig(i) < id_start .or. iorig(i) > id_end) cycle outer
             inner: do j=1,nprev
@@ -165,7 +203,7 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
                   exit inner
                endif
             enddo inner
-            if (iamtype(iphase(i)) /= iboundary .and. i > n_boundary) then ! skip boundary particles
+            if (iamtype(iphase(i)) /= iboundary) then ! skip boundary particles by particle type
                !Thermodynamic quantities
                rho_cgs = rhoh(xyzh(4,i),particlemass)*unit_density
                gammai = gamma
@@ -178,23 +216,18 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
                AV = real(av_lookup(iorig(i)))
 
             !$omp critical
-            write(filename, '(i9)') iorig(i)
-            inquire(file=trim(dir)//trim(adjustl(filename))//'.phys', size=isize)
-            if (isize == -1) then
-               open(iu, file=trim(dir)//trim(adjustl(filename))//'.phys', status='new', action='write')
-               print *, 'Creating new file for particle ', iorig(i)
-               write(iu, *) '# time(s)   X(AU)   Y(AU)   Z(AU)   n(cm-3)   T(K)   A_V'
-            else if (isize == 0) then
-               open(iu, file=trim(dir)//trim(adjustl(filename))//'.phys', status='old', action='write')
-               print *, 'Filling empty file for particle ', iorig(i)
-               write(iu, *) '# time(s)   X(AU)   Y(AU)   Z(AU)   n(cm-3)   T(K)   A_V'
-            else
-               open(iu, file=trim(dir)//trim(adjustl(filename))//'.phys', status='old', action='write', position='append')
-            endif 
-               ! write physical parameters to file
-               write(iu, '(ES16.8,1x,ES14.7,1x,ES14.7,1x,ES14.7,1x,ES14.7,1x,F8.2,1x,F10.8,1x)')&
-                time*utime, xyzh(1, i), xyzh(2, i), xyzh(3, i), numberdensity, T_gas, AV
-            close(iu)
+            batch_idx = batch_for_id(iorig(i))
+            if (batch_idx >= 0 .and. batch_idx < n_batches) then
+               pid_out = int(iorig(i), int64)
+               time_out = real(time*utime, real64)
+               x_out = real(xyzh(1, i), real64)
+               y_out = real(xyzh(2, i), real64)
+               z_out = real(xyzh(3, i), real64)
+               density_out = real(numberdensity, real64)
+               temp_out = real(T_gas, real64)
+               av_out = real(AV, real64)
+               write(batch_units(batch_idx+1)) pid_out, time_out, x_out, y_out, z_out, density_out, temp_out, av_out
+            endif
             !$omp end critical
             endif
 
@@ -209,7 +242,7 @@ subroutine do_analysis(dumpfile,num,xyzh,vxyzu,particlemass,npart,time,iunit)
       enddo outer
       print*, "=== Analysis of ", trim(dumpfile), " complete ==="
    endif
-   
+
  ! store current step data before moving on to next step
  nprev = npart
  iorig_old(1:npart) = iorig(1:npart)
